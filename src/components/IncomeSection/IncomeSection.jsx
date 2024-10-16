4fcc8462926ba5996a52aaf6e477db7a367941d5
toufiq007 <t.limon2342@gmail.com>
2024-10-17 02:17:22 +0600

    fix incomeSection sortingPopupBox and filterPoppupBox

--- src/components/IncomeSection/IncomeSection.jsx
+++ src/components/IncomeSection/IncomeSection.jsx
@@ -1,10 +1,7 @@
-import {
-  IncomeSectionIcon,
-  EditIcon,
-  DeleteIcon,
-} from "../../assets/svgicons/svgIcons";
+import { IncomeSectionIcon } from "../../assets/svgicons/svgIcons";
 import IncomeFilterSection from "./IncomeFilterSection";
 import IncomeSortingSection from "./IncomeSortingSection";
+import SingleIncome from "./SingleIncome";
 
 const IncomeSection = () => {
   return (
@@ -30,69 +27,8 @@ const IncomeSection = () => {
         </div>
 
         <div className="p-4 divide-y">
-          <div className="flex justify-between items-center py-2 relative group cursor-pointer">
-            <div>
-              <h3 className="text-base font-medium leading-7 text-gray-600">
-                Salary
-              </h3>
-              <p className="text-xs text-gray-600">15 January 2024</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <p className="text-base font-semibold text-gray-600 transition-all group-hover:-translate-x-14">
-                BDT 10000
-              </p>
-              <div className="translate-x-5 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 absolute right-0 top-1/2 -translate-y-1/2 transition-all">
-                <button
-                  className="hover:text-teal-600"
-                  role="button"
-                  title="Edit Button"
-                >
-                  <EditIcon />
-                </button>
-
-                <button
-                  className="hover:text-red-600"
-                  role="button"
-                  title="Delete"
-                >
-                  <DeleteIcon />
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex justify-between items-center py-2 relative group cursor-pointer">
-            <div>
-              <h3 className="text-base font-medium leading-7 text-gray-600">
-                Salary
-              </h3>
-              <p className="text-xs text-gray-600">15 January 2024</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <p className="text-base font-semibold text-gray-600 transition-all group-hover:-translate-x-14">
-                BDT 10000
-              </p>
-
-              {/* <!-- 3 Dots --> */}
-              <div className="translate-x-5 group-hover:translate-x-0 opacity-0 group-hover:opacity-100 absolute right-0 top-1/2 -translate-y-1/2 transition-all">
-                <button
-                  className="hover:text-teal-600"
-                  role="button"
-                  title="Edit Button"
-                >
-                  <EditIcon />
-                </button>
-
-                <button
-                  className="hover:text-red-600"
-                  role="button"
-                  title="Delete"
-                >
-                  <DeleteIcon />
-                </button>
-              </div>
-            </div>
-          </div>
+          <SingleIncome />
+          <SingleIncome />
         </div>
       </div>
     </>
